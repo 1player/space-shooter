1e0c15aac463cbbcfc24ac45b5a1c890a51b60bb
Stéphane Travostino <stephane.travostino@gmail.com>
2014-04-27 23:59:46 +0100

    Player movement, fixed direction calculations

--- player.lua
+++ player.lua
@@ -3,16 +3,21 @@ local class = require 'hump.class'
 Player = class{}
 
 local SIZE = 32
-local HSIZE = SIZE/2
-local SPEED = 10
+local SPEED = 300
+local INERTIA = 10
 local COLOR = {51, 51, 204}
+
+local HSIZE = SIZE/2
+local FRICTION = 1-(1/INERTIA)
 
 function Player:init(posv)
 	-- position vector of the center of the player
 	self.posv = posv
 
 	-- direction vector, pointing up initially
-	self.dir = vector.new(1, 0)
+	self.dir = vector.new(0, -1)
+
+	self.movev = vector.new(0, 0)
 end
 
 function Player:updateDirection(dt)
@@ -20,15 +25,25 @@ function Player:updateDirection(dt)
 	local mousePosv = vector.new(love.mouse.getPosition())
 	local mouseDirv = mousePosv - self.posv
 
-	-- rotate direction vector by 90 degrees, so that 0 is up
-	mouseDirv:rotate_inplace(math.pi/2)
-
 	-- set direction vector
 	self.dir = mouseDirv:normalized()
 end
 
+function Player:move(dt)
+	if love.keyboard.isDown("w") then
+		self.movev = vector.new(1, 1) * SPEED * dt
+	elseif love.keyboard.isDown("s") then
+		self.movev = vector.new(-1, -1) * SPEED * dt
+	else
+		self.movev = self.movev * FRICTION
+	end
+
+	self.posv = self.posv + self.dir:permul(self.movev)
+end
+
 function Player:update(dt)
 	self:updateDirection(dt)
+	self:move(dt)
 end
 
 function Player:draw()
@@ -39,7 +54,7 @@ function Player:draw()
 		love.graphics.translate(self.posv.x, self.posv.y)
 
 		-- rotate player by the direction
-		love.graphics.rotate(self.dir:angleTo())
+		love.graphics.rotate(self.dir:angleTo()+math.pi/2)
 
 		-- draw player triangle
 		love.graphics.polygon('fill',
